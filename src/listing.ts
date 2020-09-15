e05b396a898c7b4e3e3a47a6b8848da7b1b40470
simonzg <zhanghan.simon@gmail.com>
2020-09-15 17:02:00 +0800

    update listings with new files

--- src/listing.ts
+++ src/listing.ts
@@ -2,10 +2,17 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { parse } from 'papaparse';
 import { Listing } from './const';
-
+/**
+ * Data sources:
+ * https://github.com/datasets/s-and-p-500-companies
+ * https://github.com/datasets/nasdaq-listings
+ * https://github.com/datasets/nyse-other-listings
+ *
+ */
 export const loadListingFromFile = async (
   filename: string,
-  symbolColName: string
+  symbolColName: string,
+  companyNameColName: string
 ) => {
   return new Promise<string[]>((resolve, reject) => {
     const file = fs.readFileSync(
@@ -14,18 +21,53 @@ export const loadListingFromFile = async (
     );
     parse(file, {
       header: true,
+      error: (err) => {
+        console.log(err);
+      },
       complete: (result) => {
-        const symbols = result.data
-          .map((row) => row[symbolColName])
-          .filter((s: string) => !s.includes('^') && !s.includes('.'));
-        resolve(symbols);
+        const symSet = {};
+        const companies: { [key: string]: string[] } = {};
+        for (const row of result.data) {
+          let symbol = row[symbolColName];
+          if (!symbol) {
+            continue;
+          }
+          let name = row[companyNameColName];
+          for (const separator of ['#', '.', '^', '$']) {
+            if (symbol.includes(separator)) {
+              symbol = symbol.substr(0, symbol.indexOf(separator));
+            }
+          }
+          if (companies.hasOwnProperty(name)) {
+            companies[name].push(symbol);
+          } else {
+            companies[name] = [symbol];
+          }
+        }
+
+        for (const companySymbols of Object.values(companies)) {
+          if (companySymbols.length === 1) {
+            symSet[companySymbols[0]] = true;
+          } else {
+            const sym = companySymbols.reduce((prev, cur) => {
+              try {
+                return cur.length < prev.length ? cur : prev;
+              } catch (e) {
+                console.log('y', e);
+                return '';
+              }
+            }, 'XXXXXXXX');
+            symSet[sym] = true;
+          }
+        }
+        resolve(Object.keys(symSet));
       },
     });
   });
 };
 
 export const loadMyOwnListing = (): string[] => {
-  const filepath = path.join(__dirname, '..', 'config', 'myown_listing.json');
+  const filepath = path.join(__dirname, '..', 'config', 'myown-listing.json');
   const myown = JSON.parse(fs.readFileSync(filepath).toString());
   let symbols: { [key: string]: boolean } = {};
   for (const sector of Object.values(myown)) {
@@ -38,30 +80,57 @@ export const loadMyOwnListing = (): string[] => {
   return Object.keys(symbols);
 };
 
+export const loadIgnoreListing = (): string[] => {
+  const filepath = path.join(__dirname, '..', 'config', 'ignore-listing.json');
+  const ignores = JSON.parse(fs.readFileSync(filepath).toString());
+  return Object.keys(ignores);
+};
+
 export const loadSymbolsByListing = async (ex: Listing): Promise<string[]> => {
+  let symbols = [];
   switch (ex) {
     case Listing.Myown:
-      return loadMyOwnListing();
-    case Listing.Amex:
-      return await loadListingFromFile('AMEX.csv', 'Symbol');
+      symbols = loadMyOwnListing();
+      break;
     case Listing.Nasdaq:
-      return await loadListingFromFile('NASDAQ.csv', 'Symbol');
+      symbols = await loadListingFromFile(
+        'nasdaq-listed.csv',
+        'Symbol',
+        'Company Name'
+      );
+      break;
     case Listing.Nyse:
-      return await loadListingFromFile('NYSE.csv', 'Symbol');
+      symbols = await loadListingFromFile(
+        'nyse-listed.csv',
+        'ACT Symbol',
+        'Company Name'
+      );
+      break;
     case Listing.Russel2000:
-      return await loadListingFromFile('russel2000.csv', 'Security Ticker');
+      symbols = await loadListingFromFile(
+        'russel2000.csv',
+        'Security Ticker',
+        'Security Description'
+      );
+      break;
     case Listing.SP500:
-      return await loadListingFromFile('sp500.csv', 'Ticker');
+      symbols = await loadListingFromFile('sp500-listed.csv', 'Symbol', 'Name');
+      break;
   }
+  const ignores = new Set(loadIgnoreListing());
+  console.log(`${ex}: Loaded ${symbols.length} symbols`);
+  return symbols.filter((sym) => {
+    return !ignores.has(sym);
+  });
 };
 
 export const loadAllSymbols = async () => {
   const nasdaqSymbols = await loadSymbolsByListing(Listing.Nasdaq);
   const sp500Symbols = await loadSymbolsByListing(Listing.SP500);
+  const nyseSymbols = await loadSymbolsByListing(Listing.Nyse);
   const myOwnSymbols = await loadSymbolsByListing(Listing.Myown);
-  const amexSymbols = await loadSymbolsByListing(Listing.Amex);
   const symbolsSet = new Set(
-    nasdaqSymbols.concat(sp500Symbols).concat(myOwnSymbols).concat(amexSymbols)
+    nasdaqSymbols.concat(sp500Symbols).concat(myOwnSymbols).concat(nyseSymbols)
   );
   const symbols = [...symbolsSet];
   return symbols;
