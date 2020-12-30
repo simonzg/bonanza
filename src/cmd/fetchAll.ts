9781d6497fcc7066633393946676b02ebd6ac386
simonzg <zhanghan.simon@gmail.com>
2020-12-30 13:15:14 +0800

    update fetchAll

--- src/cmd/fetchAll.ts
+++ src/cmd/fetchAll.ts
@@ -33,18 +33,18 @@ const runCmd = async (action: Action, source: Source, models: string[], symbols:
     symbolCount: symbols.length,
   });
   // get finnhub one-time fact
-  // await runCmd(Action.fetch, Source.finnhub, ['profile', 'peer'], symbols, true, true);
+  await runCmd(Action.fetch, Source.finnhub, ['profile', 'peer'], symbols, true, true);
 
   // get finnhub day data
-  // await runCmd(Action.fetch, Source.finnhub, ['candle', 'price_target', 'recommend', 'day_indicator'], symbols, true, true);
+  await runCmd(Action.fetch, Source.finnhub, ['candle', 'price_target', 'recommend', 'day_indicator'], symbols, true, false);
 
   // get finnhub week data
-  // await runCmd( Action.fetch, Source.finnhub, ['week_indicator'], symbols, true, false);
+  await runCmd(Action.fetch, Source.finnhub, ['week_indicator'], symbols, true, false);
 
   // get finviz data
-  // await runCmd(Action.fetch, Source.finviz, ['page'], symbols, true, true);
+  await runCmd(Action.fetch, Source.finviz, ['page'], symbols, true, false);
 
   // get tipranks data
   // await runCmd(Action.fetch, Source.tipranks, ['page'], symbols, true, false);
-  await runCmd(Action.fetch, Source.finviz, ['page'], ['SPY', 'QQQ', 'DJI'], false, false);
+  await runCmd(Action.fetch, Source.finviz, ['page'], ['SPY', 'QQQ'], true, false);
 })();
